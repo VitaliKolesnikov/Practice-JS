e9b63557481f9a44f60b050d12f10001bc73402b
Vitalik Kolesnikov <VitaliKolesnikov22@gmail.com>
2022-02-03 14:27:31 +0300

    add elements in app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+'Use strict'
+
+let start = document.getElementById('start');
+console.log(start)
+let btnplus1 = document.getElementsByTagName('button')[0]
+console.log(btnplus1)
+let btnplus2 = document.getElementsByTagName('button')[1]
+console.log(btnplus2)
+let check = document.querySelector('.deposit-checkmark')
+console.log(check)
+let income = document.querySelectorAll('.additional_income-item')[0]
+console.log(income)
+let income2 = document.querySelectorAll('.additional_income-item')[1]
+console.log(income2)
+let money = document.querySelector('.budget_month-value')
+console.log(money)
+let budgetDay = document.querySelector('.budget_day-value')
+console.log(budgetDay)
+let expensesMonth = document.querySelector('.expenses_month-value')
+console.log(expensesMonth)
+let additionalIncome = document.querySelector('.additional_income-value')
+console.log(additionalIncome)
+let additionalExpenses = document.querySelector('.additional_expenses-value')
+console.log(additionalExpenses)
+let period = document.querySelector('.income_period-value')
+console.log(period)
+let targetMonth = document.querySelector('.target_month-value')
+console.log(targetMonth)
+let salary = document.querySelector('.salary-amount')
+let incomeTitle = document.querySelector('.income-title')
+let incomeAmount = document.querySelector('.income-amount')
+let expensesTitle = document.querySelector('.expenses-title')
+let expensesAmount = document.querySelector('.expenses-amount')
+let expensesItem = document.querySelector('.additional_expenses-item')
+let targetAmount = document.querySelector('.target-amount')
+let periodSelect = document.querySelector('.period-select')
